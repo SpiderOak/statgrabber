aaea174ecc822d4d621b2027ccc79833185fbf17
darrint <darrint@1c37cafc-021c-0410-8f26-823ff741108b>
2007-08-06 17:56:06 +0000

    Make statgrabber immune to statgrabber daemon outages.

--- Statgrabber.py
+++ Statgrabber.py
@@ -6,36 +6,51 @@ import time
 port = 9119
 iaddr = '127.1'
 
-# Connect to server
-try:
+def ignore_socket_error(fn):
+	def tryit(*args, **kwargs):
+		try:
+			fn(*args, **kwargs)
+		except socket.error, e:
+			pass
+	return tryit
+
+def fix_tag(fn):
+	def fix(tag, *args):
+		return fn(re.sub('\s+','_',tag), *args)
+	return fix
+		
+
+@ignore_socket_error
+def set_up_socket():
+	global sock
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.connect((iaddr,port));
-except socket.error(e):
-	print 'Error creating socket: ' + e.string
 
-def verify_tag(tag):
-	return re.sub('\s+','_',tag)
+set_up_socket()
 
+@ignore_socket_error
+@fix_tag
 def count(tag):
-	tag = verify_tag(tag)
 	sock.send(tag)
 
+@ignore_socket_error
+@fix_tag
 def average(tag,val):
-	tag = verify_tag(tag)
 	sock.send("%s %s" % (tag,val))
 
+@ignore_socket_error
+@fix_tag
 def accumulate(tag,val):
-	tag = verify_tag(tag)
 	sock.send("%s +%s" % (tag,val))
 
 # Now for some fun stuff... elapsed time statistics!
 class sgtimer:
-	t0 = 0
-	tag = None
+	@fix_tag
 	def __init__(self,tag):
 		self.tag = tag
 		self.t0 = time.time()
 	
+	@ignore_socket_error
 	def finish(self):
 		average(self.tag, time.time() - self.t0)
 
